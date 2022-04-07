dde94a68062a63b423f5bebfd4c51cebef4cef09
Andriy Kutsevol <andriykutsevol@gmail.com>
2022-04-07 17:52:56 +0300

    c

--- src/device.c
+++ src/device.c
@@ -368,7 +368,18 @@ uvc_error_t uvc_open(
   unsigned char manufacturer[200];
   libusb_get_string_descriptor(usb_devh, desc.iManufacturer, 0, manufacturer, 200);
   //printf("%s \n", manufacturer);
-  dgnetP_deviceC("device.c ::: uvc_open() ::: %.10s \n", manufacturer);
+  dgnetP_deviceC("device.c ::: uvc_open() manufacturer::: %.10s \n", manufacturer);
+
+  unsigned char product[200];
+  libusb_get_string_descriptor(usb_devh, desc.iProduct, 0, product, 200);
+  //printf("%s \n", manufacturer);
+  dgnetP_deviceC("device.c ::: uvc_open() product::: %.10s \n", product);
+
+
+  unsigned char snumber[200];
+  libusb_get_string_descriptor(usb_devh, desc.iSerialNumber, 0, snumber, 200);
+  //printf("%s \n", manufacturer);
+  dgnetP_deviceC("device.c ::: uvc_open() snumber::: %.10s \n", snumber);
 
 
   ret = uvc_open_internal(dev, usb_devh, devh);
